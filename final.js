30c4b31f12aab9e99c56f4f94c919aa8b69728f6
yueryl03 <54428709+yueryl03@users.noreply.github.com>
2020-03-29 00:43:52 +0800

    Add files via upload

---
+++ final.js
@@ -0,0 +1,168 @@
+var items1 = [];
+var re1 =0;
+
+
+while( re1 <999 ) {
+      function Do1() {
+          items1.push(document.querySelector('.word1').value);
+          document.querySelector(".things-display1").innerHTML =items1;
+          document.querySelector(".items-display1").innerHTML = items1.length;
+      };
+
+      function Delete1(){
+          items1.pop();
+          document.querySelector(".things-display1").innerHTML = items1;
+          document.querySelector(".items-display1").innerHTML = items1.length;
+        };
+
+        function Reset1(){
+          document.querySelector(".things-display1").innerHTML = items1="  ";
+          document.querySelector(".items-display1").innerHTML = items1.length=0;
+        };
+
+    re1++;
+  }
+  var items2 = [];
+  var re2 =0;
+
+
+  while( re2 <999 ) {
+        function Do2() {
+            items2.push(document.querySelector('.word2').value);
+            document.querySelector(".things-display2").innerHTML =items2;
+            document.querySelector(".items-display2").innerHTML = items2.length;
+        };
+
+        function Delete2(){
+            items2.pop();
+            document.querySelector(".things-display2").innerHTML = items2;
+            document.querySelector(".items-display2").innerHTML = items2.length;
+          };
+
+          function Reset2(){
+            document.querySelector(".things-display2").innerHTML = items2="  ";
+            document.querySelector(".items-display2").innerHTML = items2.length=0;
+          };
+
+      re2++;
+    }
+    var items3 = [];
+    var re3 =0;
+
+
+    while( re3 <999 ) {
+          function Do3() {
+              items3.push(document.querySelector('.word3').value);
+              document.querySelector(".things-display3").innerHTML =items3;
+              document.querySelector(".items-display3").innerHTML = items3.length;
+          };
+
+          function Delete3(){
+              items3.pop();
+              document.querySelector(".things-display3").innerHTML = items3;
+              document.querySelector(".items-display3").innerHTML = items3.length;
+            };
+
+            function Reset3(){
+              document.querySelector(".things-display3").innerHTML = items3="  ";
+              document.querySelector(".items-display3").innerHTML = items3.length=0;
+            };
+
+        re3++;
+      }
+      var items4 = [];
+      var re4 =0;
+
+
+      while( re4 <999 ) {
+            function Do4() {
+                items4.push(document.querySelector('.word4').value);
+                document.querySelector(".things-display4").innerHTML =items4;
+                document.querySelector(".items-display4").innerHTML = items4.length;
+            };
+
+            function Delete4(){
+                items4.pop();
+                document.querySelector(".things-display4").innerHTML = items4;
+                document.querySelector(".items-display4").innerHTML = items4.length;
+              };
+
+              function Reset4(){
+                document.querySelector(".things-display4").innerHTML = items4="  ";
+                document.querySelector(".items-display4").innerHTML = items4.length=0;
+              };
+
+          re4++;
+        }
+        var items5 = [];
+        var re5 =0;
+
+
+        while( re5 <999 ) {
+              function Do5() {
+                  items5.push(document.querySelector('.word5').value);
+                  document.querySelector(".things-display5").innerHTML =items5;
+                  document.querySelector(".items-display5").innerHTML = items5.length;
+              };
+
+              function Delete5(){
+                  items5.pop();
+                  document.querySelector(".things-display5").innerHTML = items5;
+                  document.querySelector(".items-display5").innerHTML = items5.length;
+                };
+
+                function Reset5(){
+                  document.querySelector(".things-display5").innerHTML = items5="  ";
+                  document.querySelector(".items-display5").innerHTML = items5.length=0;
+                };
+
+            re5++;
+          }
+          var items6 = [];
+          var re6 =0;
+
+
+          while( re6 <999 ) {
+                function Do6() {
+                    items6.push(document.querySelector('.word6').value);
+                    document.querySelector(".things-display6").innerHTML =items6;
+                    document.querySelector(".items-display6").innerHTML = items6.length;
+                };
+
+                function Delete6(){
+                    items6.pop();
+                    document.querySelector(".things-display6").innerHTML = items6;
+                    document.querySelector(".items-display6").innerHTML = items6.length;
+                  };
+
+                  function Reset6(){
+                    document.querySelector(".things-display6").innerHTML = items6="  ";
+                    document.querySelector(".items-display6").innerHTML = items6.length=0;
+                  };
+
+              re6++;
+            }
+            var items7 = [];
+            var re7 =0;
+
+
+            while( re7 <999 ) {
+                  function Do7() {
+                      items7.push(document.querySelector('.word7').value);
+                      document.querySelector(".things-display7").innerHTML =items7;
+                      document.querySelector(".items-display7").innerHTML = items7.length;
+                  };
+
+                  function Delete7(){
+                      items7.pop();
+                      document.querySelector(".things-display7").innerHTML = items7;
+                      document.querySelector(".items-display7").innerHTML = items7.length;
+                    };
+
+                    function Reset7(){
+                      document.querySelector(".things-display7").innerHTML = items7="  ";
+                      document.querySelector(".items-display7").innerHTML = items7.length=0;
+                    };
+
+                re7++;
+              }
